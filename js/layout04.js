12fb7b7270dfd31b4c9b276430a78df08e580c6e
KIMYERIM123 <133613841+KIMYERIM123@users.noreply.github.com>
2023-06-12 16:02:15 +0900

    Add files via upload

---
+++ js/layout04.js
@@ -0,0 +1,28 @@
+$(function () {
+
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        if (sct > 0) {
+            $('.header').addClass('on')
+        } else {
+            $('.header').removeClass('on')
+        }
+    });
+
+    $('.mainslide').slick({
+        fade: true,
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 4000,
+    });
+
+    $('.mainvisual .arrows .left').on('click', function () {
+        $('.mainslide').slick('slickPrev');
+    })
+
+    $('.mainvisual .arrows .right').on('click', function () {
+        $('.mainslide').slick('slickNext');
+    })
+
+});
